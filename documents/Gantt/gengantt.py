3de1f43a25f5f517e9d8d656bddc3fb17da0cde5
nivlekp <osamupang@gmail.com>
2020-04-10 10:50:33 +1000

    created personal access token to retrieve projects

--- documents/Gantt/gengantt.py
+++ documents/Gantt/gengantt.py
@@ -19,9 +19,9 @@ class GithubInfo():
         milestones_closed = requests.get(self.repo_addr + 'milestones?state=closed')
         mjson = milestones.json()
         mcljson = milestones_closed.json()
-        self.mjson_all = mjson + mcljson
+        self.mjson = mjson + mcljson
         print('Retrieved milestones.')
-        # for m in self.mjson_all:
+        # for m in self.mjson:
         #     print('milestone:', m['title'])
 
     def get_issues(self):
@@ -29,14 +29,34 @@ class GithubInfo():
         issues_closed = requests.get(self.repo_addr + 'issues?q=is%3Aissue+is%3Aclosed')
         ijson = issues.json()
         icljson = issues_closed.json()
-        self.ijson_all = ijson + icljson
+        self.ijson = ijson + icljson
         print('Retrieved issues.')
-        # for i in self.ijson_all:
+        # for i in self.ijson:
         #     print('issue:', i['title'])
 
+    def get_proj(self):
+        headers={'Accept':'application/vnd.github.inertia-preview+json',\
+            'Authorization': 'token ' + self.token}
+        projects = requests.get(self.repo_addr + 'projects', headers=headers)
+        projects_closed = requests.get(self.repo_addr + 'projects?query=is%3Aclosed',\
+            headers=headers)
+        pjson = projects.json()
+        pcljson = projects_closed.json()
+        self.pjson = pjson + pcljson
+        print('Retrieved projects.')
+
+    def get_auth(self):
+        self.user = 'tszkiup'
+        self.token = '96fa62363797672f5141bd789d325ce9495907a6'
+        #self.user = input('Please enter username: ')
+        #self.password = getpass(prompt='Please enter your password: ')
+        #self.token = getpass(prompt='Please enter personal token: ')
+
     def get_json_from_github(self):
+        self.get_auth()
         self.get_mstone()
         self.get_issues()
+        self.get_proj()
 
 class ExtraInfo():
     def __init__(self):
@@ -45,21 +65,22 @@ class ExtraInfo():
         self.cap_dur = self.cap_end_date - self.cap_start_date
 
 class TexGen():
-    def __init__(self):
+    def __init__(self, info):
         self.filename = 'gantt.tex'
         self.texfile = open(self.filename, 'w')
         self.indent = 0
         self.cap_start_date = datetime.date(2020, 3, 2)
         self.cap_end_date = datetime.date(2020, 11, 27)
+        self.info = info
         self.einfo = ExtraInfo()
 
-    def gen_gantt(self, info):
+    def gen_gantt(self):
         '''
         method for generating the whole document
         '''
         self.gen_config()
         self.begin_doc()
-        self.gen_gantt_chart(info)
+        self.gen_gantt_chart()
         self.end_doc()
         self.texfile.close()
         self.gen_pdf()
@@ -80,17 +101,17 @@ class TexGen():
     def end_doc(self):
         self.print2tex('\\end{document}')
 
-    def gen_gantt_chart(self, info):
+    def gen_gantt_chart(self):
         '''
         method for generating the gantt chart
         '''
         self.print_line('\\begin{ganttchart}')
-        self.gantt_load_config(info)
-        self.gantt_gen_title(info)
-        # self.load_task(info)
+        self.gantt_load_config()
+        self.gantt_gen_title()
+        self.gantt_gen_tasks()
         self.print_line('\\end{ganttchart}')
 
-    def gantt_load_config(self, info):
+    def gantt_load_config(self):
         self.print_line('[')
         self.add_indent()
         self.print_line('x unit=0.1cm,')
@@ -99,7 +120,7 @@ class TexGen():
         self.rm_indent()
         self.print_line(']{1}{' + str(self.einfo.cap_dur.days+1) + '}')
 
-    def gantt_gen_title(self, info):
+    def gantt_gen_title(self):
         pf = '\\gantttitle'
         sdate = self.cap_start_date
         edate = self.cap_end_date
@@ -112,6 +133,11 @@ class TexGen():
                 +str(calendar.monthrange(2020, m)[1])+'}')
         self.print_line(pf+'{'+calendar.month_name[edate.month]+'}{'\
             +str(edate.day)+'}')
+
+    def gantt_gen_tasks(self):
+        pf = '\\ganttbar'
+        sdate = self.cap_start_date
+        edate = self.cap_end_date
 
     def print_line(self, string):
         self.print2tex(' '*self.indent + string)
@@ -126,14 +152,14 @@ class TexGen():
         self.indent -= 4
 
     def gen_pdf(self):
-        subprocess.run(['pdflatex', self.filename])
-        os.remove(os.path.splitext(self.filename)[0] + '.log')
-        os.remove(os.path.splitext(self.filename)[0] + '.aux')
-        print('Removed .log and .aux files.')
+        # subprocess.run(['pdflatex', self.filename])
+        # os.remove(os.path.splitext(self.filename)[0] + '.log')
+        # os.remove(os.path.splitext(self.filename)[0] + '.aux')
+        # print('Removed .log and .aux files.')
         print('Finished.')
 
 if __name__ == "__main__":
     info = GithubInfo()
     info.get_json_from_github()
-    texgen = TexGen()
-    texgen.gen_gantt(info)
+    texgen = TexGen(info)
+    texgen.gen_gantt()
