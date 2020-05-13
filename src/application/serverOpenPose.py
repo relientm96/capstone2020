f826f17a9a1e8a75aebd2b71b898ed86f2cb8e56
Matthew Fen Yi Yong <yongf@student.unimelb.edu.au>
2020-05-14 03:24:20 +1000

    Adding project files to src/application

--- src/application/serverOpenPose.py
+++ src/application/serverOpenPose.py
@@ -15,34 +15,26 @@ import gestureRecognition as gr
 
 # Hold parameters for OpenPose as a dictionary
 params = dict()
-
 ###### SET OPEN POSE FLAGS HERE ############
 # Custom Params 
 # (refer to https://github.com/CMU-Perceptual-Computing-Lab/openpose/blob/master/include/openpose/flags.hpp
 # for more parameters)
-params["model_folder"] = "../../../models/"
 params["net_resolution"] = "320x320"
 params["hand"] = True
 params["hand_net_resolution"] = "328x328"
 
 ############################################
 
+# Importing OpenPose 
 try:
-    # Import Openpose (Windows/Ubuntu/OSX)
     dir_path = os.path.dirname(os.path.realpath(__file__))
     try:
         # Windows Import
         if platform == "win32":
             # Change these variables to point to the correct folder (Release/x64 etc.)
-            sys.path.append(dir_path + '/../../python/openpose/Release');
-            os.environ['PATH']  = os.environ['PATH'] + ';' + dir_path + '/../../x64/Release;' +  dir_path + '/../../bin;'
+            sys.path.append(dir_path + '/Release');
+            os.environ['PATH']  = os.environ['PATH']  + ';' + dir_path + "/bin" 
             import pyopenpose as op
-        else:
-            # Change these variables to point to the correct folder (Release/x64 etc.)
-            sys.path.append('../../python');
-            # If you run `make install` (default path is `/usr/local/python` for Ubuntu), you can also access the OpenPose/python module from there. This will install OpenPose and the python library at your desired installation path. Ensure that this is in your python path in order to use it.
-            # sys.path.append('/usr/local/python')
-            from openpose import pyopenpose as op
     except ImportError as e:
         print('Error: OpenPose library could not be found. Did you enable `BUILD_PYTHON` in CMake and have this Python script in the right folder?')
         raise e
@@ -78,6 +70,11 @@ except Exception as e:
     sys.exit(-1)
 
 def processFrames(inputImageUri):
+    '''
+    Receives input image url from browser as a base64 string
+    Decodes as numpy image array and processes using openpose
+    Returns encoded base64 url string for processed openpose image
+    '''
     try:
         b64_string = inputImageUri.split(',')[0]
         b64_string += "=" * ((4 - len(b64_string) % 4) % 4)
@@ -102,11 +99,6 @@ def processFrames(inputImageUri):
         thickness = 2
         image = cv2.putText(image, word, org, font,  
                         fontScale, color, thickness, cv2.LINE_AA)
-                
-        # Display Rendered Poses + Word On OpenCV GUI
-        #cv2.imshow("CAPSTONE 2020, Sign Language Translation System", image)
-        # Wait 1ms after processing to display image to OpenCV2's GUI Window
-        #cv2.waitKey(1)
 
         # Encode Image in base64
         retval, buffer = cv2.imencode('.jpg', image)
@@ -120,23 +112,4 @@ def processFrames(inputImageUri):
         sys.exit(-1)
         return "noimage"
 
-#cap = cv2.VideoCapture(0)
-
-#
-#cap = cv2.VideoCapture(0)
-#while True:
-#    try:
-#        ret,frame = cap.read()
-#        result = processFrames(frame)
-#        # print(result)
-#        # Convert back to binary
-#        jpg_original = base64.b64decode(result)
-#        # Write to a file to show conversion worked
-#        jpg_as_np = np.frombuffer(jpg_original, dtype=np.uint8)
-#        image_buffer = cv2.imdecode(jpg_as_np, flags=1)
-#        print(image_buffer)
-#
-#    except Exception as e:
-#       print(e)
-#       sys.exit(-1)
         
