9b419e788e631fbda49bb5cf2a3b1c7731ce9c81
yick <yongw4@student.unimelb.edu.au>
2020-07-18 00:49:08 +1000

    tested; still need to add some safe guards

--- src/training/images2txt_db.py
+++ src/training/images2txt_db.py
@@ -13,6 +13,9 @@
 import images2json as i2j
 import json_image2txt as ji2t
 import tempfile # needed for temporary files/directory;
+from pprint import pprint
+import glob, os
+
 
 	
 # assume this image format to be fixed throughout; 
@@ -20,46 +23,52 @@ import tempfile # needed for temporary files/directory;
 imageformat = "png"
 	
 def images2txt_db(imagfolder_path, db_path, imageformat):
-    '''
-    args:
-        1. imagfolder_path; the folder with all the raw images; e.g.
-            #   folder: alphabet-1
-            #       images: 
-            #          -> A1.png
-            #           ...
-            #          -> Z1.png
-        2. db_path; save all the converted image_txt to ...?
-    return:
-        none;
-    function:
-        1. # to create a database of all the reformatted images in txt;
-    '''
-    
-    # create a directory to store all the json files;
-    # once all images have been processed; 
-    # convert these json to txt and save it to the final destionation
-    dummydirec = tempfile.TemporaryDirectory()
-    dummy_path = dummydirec.name
-    image2json(src_path, dummy_path, imageformat)
+	'''
+	args:
+		1. imagfolder_path; the folder with all the raw images; e.g.
+			#   folder: alphabet-1
+			#       images: 
+			#          -> A1.png
+			#           ...
+			#          -> Z1.png
+		2. db_path; save all the converted image_txt to ...?
+	return:
+		none;
+	function:
+		1. # to create a database of all the reformatted images in txt;
+	'''
+	
+	# create a temporary directory to store all the json files;
+	# once all images have been processed; 
+	# convert these json to txt and save it to the final destionation
+	with tempfile.TemporaryDirectory() as dummy_direc:
+		#print(dummy_direc)
+		i2j.images2json(imagfolder_path, dummy_direc, imageformat)
 
-
-
-
-
-    # reminder; once all done; delete the temp folder for good practice;
-    # note: once this script is done running; the temp direc will be deleted regardless;
-    dummydirec.cleanup()
-
-    # test driver;
+		# append the regex
+		dummy_direc = dummy_direc + "\\*.json"
+		for imagepath in sorted(glob.glob(os.path.join(dummy_direc))):
+			print(imagepath)
+			# get the class name and append it to the txt name so that
+			# it's easier to generate the labelling;
+			tmpstring = (imagepath.split('\\'))[-1]
+			tmpstring = (tmpstring.split('.'))[0]
+			txtname = (tmpstring.split('_'))[0]
+			save_path = db_path + txtname + '.txt'
+			print(save_path)
+			ji2t.json_image2txt(imagepath, save_path, 32)
+			print("individual txt is being created and saved;\n")
+		print("all json images of the same class have been saved as txt")
+		
+	
+# test driver;
 if __name__ == '__main__':
-    N = 5
-    # fixed location for all converted images;
-        write_path = "C:\\Users\\yongw4\\Desktop\\image-database\\"
-    
-    for i in range(1, N):
-        tmp = "C:\\Users\\yongw4\\Desktop\\alphabets\\alphabets-{CHANGE}\\"
-        imageformat = "png"
-        src_path = tmp.format(CHANGE = i)
-        print(src_path)
-        
-        
+	N = 5
+	# fixed location for all converted images;
+	write_path = "C:\\Users\\yongw4\\Desktop\\image-database\\"
+	# run through all alphabet folders;
+	for i in range(1, N):
+		tmp = "C:\\Users\\yongw4\\Desktop\\alphabets\\alphabets-{CHANGE}\\"
+		imageformat = "png"
+		src_path = tmp.format(CHANGE = i)
+		images2txt_db(src_path, write_path, "png")
