d5ef43885d7ad6ea344fee2bbbdd922ba9e86ba5
yick <yongw4@student.unimelb.edu.au>
2020-08-15 23:26:24 +1000

    ready to run

--- src/training/run_OP_videos.py
+++ src/training/run_OP_videos.py
@@ -9,9 +9,11 @@ def process_one_video(input, path_X, path_Y):
 		syntools.synthesize_block(input, speed_seed, path_X, path_Y)
 
 if __name__ == '__main__':
-	path_X = "C:\\Users\\yongw4\\Desktop\\test_synthesis\\X_dummy.txt"
-	path_Y = "C:\\Users\\yongw4\\Desktop\\test_synthesis\\Y_dummy.txt"
-	signvideodirectory = "C:\\Users\\yongw4\\Desktop\\AUSLAN-DUMMY"
+	signvideodirectory = "C:\\Users\\yongw4\\Desktop\\FATE"
+	
+	path_X = os.path.join(signvideodirectory, "X_train.txt")
+	path_Y = os.path.join(signvideodirectory, "Y_train.txt")
+	
 	# safeguard;
 	# create them if the files do not exist;
 	print("Checking if the X.txt and Y.txt exist ...\n")
@@ -23,7 +25,6 @@ if __name__ == '__main__':
 			print("An error occured", e)
 			sys.exit(-1)
 
-
 	for root, dirs, files in os.walk(signvideodirectory, topdown=False):
 		for name in files:
 			# make sure the current file is MP4;
@@ -33,11 +34,11 @@ if __name__ == '__main__':
 			# OK, it's mp4; process it;
 			src_path = os.path.join(root, name)
 			
-            # current video has not been processed; 
+			# current video has not been processed; 
 			if not (ftools.checksubstring(src_path, "checked")):
 				process_one_video(src_path, path_X, path_Y)
 
-                # done processing? sign off;
+				# done processing? sign off;
 				# so that the processed video will not be processed again;
 				print('the current video has been processed\n')
 				[_, dst_checked] = ftools.checkoff_file(src_path, "checked")
