eb9b6a2200943a7d1591c03977b55df58bd783dd
Matthew Yong <matthewyfy@gmail.com>
2020-08-28 19:32:13 +1000

    Testing markdown rendering

--- docs/js/docs.js
+++ docs/js/docs.js
@@ -1,7 +1,24 @@
+function fileToMarked(file, element){
+    $.ajax({
+        url: file,
+        crossDomain: true,
+        beforeSend: function(request) {
+            request.setRequestHeader("Access-Control-Allow-Origin", "*");
+            request.setRequestHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+            request.setRequestHeader("Access-Control-Allow-Headers", "X-PINGOTHER, Content-Type");
+        },
+        success: function(response){
+            console.log(JSON.parse(response))
+            element.innerhtml = data;
+        }
+    })
+}
 
 $(document).ready(function(){
     $('.sidenav').sidenav();
     $('.scrollspy').scrollSpy();
     $('.materialboxed').materialbox();
 
+    overview_elem = document.getElementById('overview')
+    fileToMarked("markdown/overview.md", overview_elem);    
 });
---
+++ docs/markdown/overview.md
@@ -0,0 +1,4 @@
+# Overview
+
+* Point 1
+* Point 2
