aeaca426ebfef9361bb39b1e189f29b4c2aca3ee
yick <yongw4@student.unimelb.edu.au>
2020-08-31 18:49:03 +1000

    restructure using hyperopt

--- src/training/LSTM.py
+++ src/training/LSTM.py
@@ -1,147 +1,140 @@
+
 from __future__ import print_function
 
 # for hyperparameter tuning;
-from hyperopt import Trials, STATUS_OK, tpe
 from hyperas import optim
 from hyperas.distributions import choice, uniform
+from hyperopt import fmin, tpe, hp, STATUS_OK, Trials
 
 from tensorflow.keras.models import Sequential
 from tensorflow.keras.layers import Dense, Dropout, LSTM, Activation
-from tensorflow.keras import backend as keras_backend
 import tensorflow as tf
 from sklearn.model_selection import train_test_split
 from sklearn.model_selection import StratifiedKFold
 
+import sys
 import tracemalloc
 import LSTM_tools as lstm
 import numpy as np
-import sys
 
-# global variables
-xtrain_path = "./training-files/X_train.txt"
-ytrain_path = "./training-files/Y_train.txt"
 
-def data_feed():
-	"""
-	Data providing function:
+# hardcoding for now;
+n_classes = 5
+txt_directory = "C:\\Users\\yongw4\\Desktop\\FATE\\txt-files\\speed-01"
+X_monstar, Y_monstar = lstm.patch_nparrays(txt_directory)
 
-	This function is separated from create_model() so that hyperopt
-	won't reload data for each evaluation run.
-	"""
-	xtrain_path = "./training-files/X_train.txt"
-	ytrain_path = "./training-files/Y_train.txt"
-
-	X_data = lstm.load_X(xtrain_path)
-	Y_data = lstm.load_Y(ytrain_path)
-	# CAUTION, we need to stratify as we have an imbalanced dataset;
+# seprate into training and validation set for hyperparameter tuning;
+# some notes:
+# 1. since we are doing hyperparameter tuning, we need not concern about bias or variance;
+# 1.1 such will be dealt when we train the model with the obtained hyperparameters using cross-fold val;
+# 2. we need to stratify as we have an imbalanced dataset;
 	# some technique besides stratification;
 	# src - https://datascience.stackexchange.com/questions/32818/train-test-split-of-unbalanced-dataset-classification
-	x_train, x_test, y_train, y_test =  train_test_split(X_data, Y_data, test_size=0.2, random_state=42, shuffle = True, stratify = Y_data)
-	return x_train, x_test, y_train, y_test
 
-def create_model(x_train, y_train, x_test, y_test):
-	"""
-	Model providing function:
+x_train, x_val, y_train, y_val =  train_test_split(X_monstar, Y_monstar, test_size=0.2, random_state=42, shuffle = True, stratify = Y_monstar)
 
-	Create Keras model with double curly brackets dropped-in as needed.
-	Return value has to be a valid python dictionary with two customary keys:
-		- loss: Specify a numeric evaluation metric to be minimized
-		- status: Just use STATUS_OK and see hyperopt documentation if not feasible
-	The last one is optional, though recommended, namely:
-		- model: specify the model just created so that we can later use it again.
+# the seach (hypercubes) space for all the hyperparameters;
+# note; discard the use of hp.uniform as this is continuous;
+# so we will have an infinite-dimensional search space;
+# it's most likely we are able to cover a depressingly tiny-fraction of the space;
+# discretise the hyperparameters;
+space = {'choice': hp.choice('num_layers',
+					[ {'layers':'two', },
+					{'layers':'three',
+					'units3': hp.choice('units3', [32, 64,128,256,512,1024]),
+					'dropout3': hp.choice('dropout3',[0,0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9,1])}
+					]),
+			'units1': hp.choice('units1', [32, 64, 128, 256, 512, 1024]),
+			'dropout1': hp.choice('dropout1', [0.1,0.2,0.3,0.4,0.5,0.6,0.7,0.8,0.9]),
+			'batch_size' : hp.choice('batch_size', [32, 64, 128]),
+			'nb_epochs' :  hp.choice('epochs', [50, 70, 80, 100]),
+			'activation': hp.choice('activation', ['tanh', 'sigmoid', 'relu'])
+		}
 
-        acknowledgement - https://github.com/maxpumperla/hyperas
-	"""
+# the objective function to be minimized for the search space;
+def f_nn(params):   
+	
 	model = Sequential()
 	model.add(LSTM(32, input_shape=(x_train.shape[1], x_train.shape[2]), return_sequences=True))
-	model.add(Activation({{choice(['relu', 'sigmoid'])}}))
-	model.add(Dropout({{uniform(0, 1)}}))
+	model.add(Activation(params['activation']))
+	model.add(Dropout(params['dropout1']))
 	
 	# If we choose 'three', add an additional third layer
-	if {{choice(['two', 'three'])}} == 'three':
-		model.add(LSTM({{choice([32, 64, 128, 256, 512])}}, return_sequences=True))
-		model.add(Activation({{choice(['relu', 'sigmoid'])}}))
-		model.add(Dropout({{uniform(0, 1)}}))
+	if (params['choice']['layers']== 'three'):
+		model.add(LSTM(int(params['choice']['units3']), return_sequences=True))
+		model.add(Activation(params['activation']))
+		model.add(Dropout(params['choice']['dropout3']))
 	
 	# last layer;
-	model.add(LSTM({{choice([32, 64, 128, 256, 512])}}))
-	model.add(Activation({{choice(['relu', 'sigmoid'])}}))
-	model.add(Dropout({{uniform(0, 1)}}))
-	model.add(Dense(4, activation='softmax'))
+	model.add(LSTM(params['units1']))
+	model.add(Activation(params['activation']))
+	model.add(Dropout(params['dropout1']))
+	model.add(Dense(n_classes, activation='softmax'))
 	
 	# does not have high impact on the performance;
 	# so not needed for tuning;
 	opt = tf.keras.optimizers.Adam(lr=1e-4, decay=1e-5)
 	
-    # done;
+	# done;
 	# objective, maximize the accuracy;
 	model.compile(loss='sparse_categorical_crossentropy', metrics=['accuracy'],
 				  optimizer=opt)
 	model.summary()
-	print((x_train.shape, y_train.shape))
-
-
-    # TODO: need to check how keras split for the validation;
-    # i.e. model.fit( .... validation_split = 0.1)
-
+	
 	result = model.fit(x_train, y_train,
-			  batch_size={{choice([32, 64, 128])}},
-			  epochs = {{choice([50, 70, 80, 100])}},
+			  batch_size=params['batch_size'],
+			  epochs = params['nb_epochs'],
 			  verbose=2,
-			  validation_split=0.1)
+			  validation_data = (x_val, y_val))
+
 	# clear tensorflow state to prevent memory overloading;
 	keras_backend.clear_session()
+
 	#get the highest validation accuracy of the training epochs
 	validation_acc = np.amax(result.history['val_accuracy']) 
 	print('Best validation acc of epoch:', validation_acc)
 	return {'loss': -validation_acc, 'status': STATUS_OK, 'model': model}
 
-# test driver;
 if __name__ == '__main__':
+
+	# note on "max_evals"
+	# by default, TPE selects the first 20 hyperparameters randomly as a warm up to get an idea of where to start.
+	# If you only try 10 configurations they will all be chosen randomly, which explains why your results are so different each time.
+	# src - https://github.com/hyperopt/hyperopt/issues/642
+
+
+	# safeguard for large number of "max_evals";
+	# running the evaluation iteratively and saving the results each step. 
+	# This way you don't need to choose how long to run it for ahead of time and can stop and continue where you left off if needed
+
+	reload_results = False
+	# Initialize an empty trials database, or reload where you left off
+	if reload_results == True:
+		trials = pickle.load(open("hyper_results.pkl", "rb"))
+	else:
+		trials = Trials()
+
+	# now; start the tuning;
+	step = 5
+	start = 20
+	end = start*10
+	'''
+	for i in range(start, end, step):
+		# fmin runs until the trials object has max_evals elements in it, so it can do evaluations in chunks like this
+		best = fmin(f_nn, space, algo=tpe.suggest, trials=trials, max_evals=i)
+		# each step 'best' will be the best trial so far
+		print(best)
+		# each step 'trials' will be updated to contain every result
+		# you can save it to reload later in case of a crash, or you decide to kill the script
+		pickle.dump(trials, open("hyper_results.pkl", "wb"))
+	'''
+	best = fmin(f_nn, space, algo=tpe.suggest, trials=trials, max_evals=1)
+	# each step 'best' will be the best trial so far
+	print(best)
+	# each step 'trials' will be updated to contain every result
+	# you can save it to reload later in case of a crash, or you decide to kill the script
+	pickle.dump(trials, open("hyper_results.pkl", "wb"))
+
+
+
 	
-	X_data = lstm.load_X(xtrain_path)
-	Y_data = lstm.load_Y(ytrain_path)
-	print(Y_data.shape)
-	print(X_data.shape)
-	
-	# test 01 - split the data into training and validation
-	# note, we have an imbalanced dataset, so stratify it;
-	X_train, X_test, Y_train, Y_test = data_feed()
-	print("X: ", X_train.shape)
-	print("Y: ", Y_train.shape)
-	print("X: ", X_test.shape)
-	print("Y: ", Y_test.shape)
-	
-	# test 02 - hyperparameter tuning'
-
-    # ***************************
-    # summary of the structure;
-    # ***************************
-
-    # hyperparameters of interest for tuning;
-    # 1. hidden unit = {32, 64, 128, 256, 512}
-    # 2. number of layers = {2, 3}
-    # 3. dropout = Uniform(0,1); note it's variational
-    # 4. Activation = {'relu', 'sigmoid'}
-    # 5. batch_size={32, 64, 128}
-	# 6. epochs = {50, 70, 80, 100, 150}
-
-    # fixed hyperparameters;
-    # 1. learning rate;
-    # 2. decay rate;
-    # 3. loss='sparse_categorical_crossentropy'
-    # 4. optimizer; (Adam)
-
-    # objective; maximize validation accuracy;
-    # hyperparameter optimizer: Tree-Parzen Estimators (TPE) - bayesian type;
-
-	best_run, best_model = optim.minimize(model=create_model,
-										  data=data_feed,
-										  algo=tpe.suggest,
-										  max_evals=5,
-										  trials=Trials())
-	X_train, Y_train, X_test, Y_test = data_feed()
-	print("Evalutation of best performing model:")
-	#print(best_model.evaluate(X_test, Y_test))
-	print("Best performing model chosen hyper-parameters:")
-	print(best_run)
