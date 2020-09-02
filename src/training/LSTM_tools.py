4a8ad40f16810e45c0e6cb449f1e36746c56ea80
yick <yongw4@student.unimelb.edu.au>
2020-09-02 19:30:38 +1000

    moved some modules around

--- src/training/LSTM_tools.py
+++ src/training/LSTM_tools.py
@@ -28,7 +28,7 @@ import tensorflow as tf
 # global constants
 n_steps = 75 
 n_hidden = 34 # Hidden layer num of features
-n_classes = 4
+n_classes = 5
 batch_size = 64
 
 #---------------------------------------------------------------------------------------------
@@ -195,35 +195,6 @@ def get_deployable_model(x_raw, y_raw):
 	# define checkpoints to get the best model locally during training;
 
 
-
-def patch_nparrays(txt_directory):
-	'''
-		task:
-			load all the generated txt files as np arrays and combine into one;
-		args:
-			txt_directory; the directory where all the txt files are stored;
-		returns:
-			a tuple of (X, Y)
-	'''
-	PATCH = [[],[]]
-	for root, dirs, files in os.walk(txt_directory, topdown=False):
-		for index, name in enumerate(files):
-			src_path = os.path.join(root, name)
-			# the X file;
-			if(index == 0):
-				dataset = load_X(src_path)
-			# the Y label;
-			else:
-				dataset = load_Y(src_path)
-			# done selecting the files?
-			PATCH[index].append(dataset)
-
-	# concatenate all the arrays into one;
-	X_monstar = np.concatenate(tuple(PATCH[0]), axis = 0)
-	Y_monstar = np.concatenate(tuple(PATCH[1]), axis = 0)
-	return (X_monstar, Y_monstar)
-
-
 # test driver;
 if __name__ == '__main__':
 	xtrain_path = "./training_files/X_train.txt"
@@ -244,10 +215,4 @@ if __name__ == '__main__':
 	cross_validate(xtrain_path, ytrain_path, kfold)
 	'''
 
-	# test - 03;
-	# patch_nparrays;
-	txt_directory = "C:\\Users\\yongw4\\Desktop\\FATE\\speed-01"
-	X_monstar, Y_monstar = patch_nparrays(txt_directory)
-	print(X_monstar.shape)
-	print(Y_monstar.shape)
 		
--- src/training/file_tools.py
+++ src/training/file_tools.py
@@ -113,6 +113,7 @@ def append_file(src, dst):
 			fileX.write(line)
 		print("finished appending\n")
 
+
 # create the file within the temp directory;
 def check_newfile(file_path):
 	if not (os.path.exists(file_path)):
@@ -122,6 +123,33 @@ def check_newfile(file_path):
 			print("An error occured", e)
 			sys.exit(-1)
 
+# combine all the np arrays;
+def patch_nparrays(txt_directory):
+	'''
+		task:
+			load all the generated txt files as np arrays and combine into one;
+		args:
+			txt_directory; the directory where all the txt files are stored;
+		returns:
+			a tuple of (X, Y)
+	'''
+	PATCH = [[],[]]
+	for root, dirs, files in os.walk(txt_directory, topdown=False):
+		for index, name in enumerate(files):
+			src_path = os.path.join(root, name)
+			# the X file;
+			if(index == 0):
+				dataset = load_X(src_path)
+			# the Y label;
+			else:
+				dataset = load_Y(src_path)
+			# done selecting the files?
+			PATCH[index].append(dataset)
+
+	# concatenate all the arrays into one;
+	X_monstar = np.concatenate(tuple(PATCH[0]), axis = 0)
+	Y_monstar = np.concatenate(tuple(PATCH[1]), axis = 0)
+	return (X_monstar, Y_monstar)
 
 # save and load the huge numpy array;
 def npy_write(data, filename):
@@ -132,9 +160,10 @@ def npy_read(filename):
 
 # test driver;
 if __name__ == '__main__':
-	dummy_directory = "C:\\Users\\yongw4\\Desktop\\dummy-directory"
 	
-	for f in os.listdir(dummy_directory):
-		src_path = dummy_directory + "\\" + f
-		track_count(src_path, saved_path = "", save_name = 'saved_counter.p')
+    # patch_nparrays;
+	txt_directory = "C:\\Users\\yongw4\\Desktop\\FATE\\speed-01"
+	#X_monstar, Y_monstar = patch_nparrays(txt_directory)
+	#print(X_monstar.shape)
+	#print(Y_monstar.shape)
 	
