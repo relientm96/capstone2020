22c62a387c4c71ed65b923c2aedc19f27379cfb9
yick <yongw4@student.unimelb.edu.au>
2020-10-01 05:52:05 +1000

    added human_detection; not tested yet

--- src/training/human-detection/detection.py
+++ src/training/human-detection/detection.py
@@ -1,5 +1,5 @@
 # src = https://towardsdatascience.com/object-detection-with-10-lines-of-code-d6cb4d86f606
-
+# needed to crop human for data augmentation;
 from imageai.Detection import ObjectDetection
 import os
 
