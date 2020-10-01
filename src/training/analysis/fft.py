2a0a0e86e163dc9f0fcc026b6b30d20c964f4f4a
yick <yongw4@student.unimelb.edu.au>
2020-10-01 13:28:30 +1000

    added fft for image; not tested yet

---
+++ src/training/analysis/fft.py
@@ -0,0 +1,22 @@
+
+# src - https://stackoverflow.com/questions/21362843/interpret-numpy-fft-fft2-output
+
+
+import matplotlib as ml
+import matplotlib.pyplot as plt
+import numpy as np
+import Image
+file_path = "data"
+image = np.asarray(Image.open(file_path).convert('L'))
+freq = np.fft.fft2(image)
+freq = np.abs(freq)
+
+fig, ax = plt.subplots(nrows=2, ncols=2, figsize=(14, 6))
+ax[0,0].hist(freq.ravel(), bins=100)
+ax[0,0].set_title('hist(freq)')
+ax[0,1].hist(np.log(freq).ravel(), bins=100)
+ax[0,1].set_title('hist(log(freq))')
+ax[1,0].imshow(np.log(freq), interpolation="none")
+ax[1,0].set_title('log(freq)')
+ax[1,1].imshow(image, interpolation="none")
+plt.show()
