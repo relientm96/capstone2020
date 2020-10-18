251e1473d8e57d2a9e3c709f9ca3ffcd2664bcb9
yick <yongw4@student.unimelb.edu.au>
2020-10-18 16:50:42 +1100

    for experimental ground

--- src/training/play.py
+++ src/training/play.py
@@ -3,83 +3,86 @@
 import cv2
 import numpy as np
 
-import tensorflow as tf
+#import tensorflow as tf
+
+import os
 from matplotlib import image
 import matplotlib.pyplot as plt
 
-path = "C:\\CAPSTONE\\capstone2020\\src\\training\\test-images\\adult_image.jpg"
-#image = cv2.imread(path,1)
-image = image.imread(path)
-print(image.dtype)
-print(image.shape)
-# display the array of pixels as an image
-plt.imshow(image)
-plt.show()
+path = "C:\\CAPSTONE\\capstone2020\\src\\training\\test-images\\test1.jpg"
 
-import cv2
- 
-# read image
-img = cv2.imread(path, cv2.IMREAD_UNCHANGED)
-plt.imshow(img)
-plt.show()
-# get dimensions of image
-dimensions = img.shape
- 
-# height, width, number of channels in image
-height = img.shape[0]
-width = img.shape[1]
-channels = img.shape[2]
- 
-print('Image Dimension    : ',dimensions)
-print('Image Height       : ',height)
-print('Image Width        : ',width)
-print('Number of Channels : ',channels)
+path = "C:\\Users\\yongw4\\Desktop\\test"
+# to count the number of subdirectories at the top layer;
+def subdir_count(path):
+	count = 0
+	for root, dirs, files in os.walk(path):
+		for i in range(len(dirs)):
+			classvideo = os.path.join(root, dirs[i])
+			print(classvideo)
+		break
+	
+# test driver;
+if __name__ == '__main__':
+	subdir_count(path)
 
 
-#transformation = tf.keras.preprocessing.image.apply_affine_transform(image,shear=10)
-transformation = tf.keras.preprocessing.image.apply_affine_transform(
-image,
-theta=0, tx=0, ty=0, shear=10, zx=1, zy=1, row_axis=1, col_axis=1,
-    channel_axis=2, fill_mode='wrap', cval=0.0, order=3)
+    
+def transform_block(func, input, storage_path, DEGREE):
+	print("\n video input: ", input)
+	# extract the info from the input pathname;
+	tmp = input.split('.')
+	prefix = tmp[0]
+	token = (prefix.split('\\'))[-1]
+	suffix = token.split('_')
+	classname = suffix[0]
+	speedname = suffix[1]
 
-transformation = tf.keras.preprocessing.image.apply_affine_transform(
-image,
-theta=0, tx=0, ty=0, shear=30, zx=1.1, zy=1.1, row_axis=0, col_axis=0,
-    channel_axis=2, fill_mode='nearest', cval=0.0, order=3)
+	# rotation degree;
+	#DEGREE = [0, 3, 5, 7, 9, -3, -5, -7, -9]
+	
+	# assigning addresses for each rotation for convenienece;
+	addresses = assign_video_locations(DEGREE, storage_path, classname, speedname, "r")
+	#print('\n assigned address: ', addresses)
+	num_workers = mp.cpu_count()  
+	#print("number of cpu cores: ", num_workers)
+	pool = mp.Pool(num_workers)
+	for index in range(len(DEGREE)):
+		print(index)
+		pool.apply_async(func, args=(input, addresses[index], DEGREE[index]))
+	print('pooled')
+	pool.close()
+	pool.join()
+	# end of function;
 
-plt.imshow(transformation)
-plt.show()
+def flip_and_transform_block(func, input, storage_path, DEGREE):
+	# extract the info from the input pathname;
+	tmp = input.split('.')
+	prefix = tmp[0]
+	token = (prefix.split('\\'))[-1]
+	suffix = token.split('_')
+	classname = suffix[0]
+	speedname = suffix[1]
 
-transformation = tf.keras.preprocessing.image.apply_affine_transform(
-image,
-theta=0, tx=0, ty=0, shear=30, zx=1.1, zy=1.1, row_axis=0, col_axis=0,
-    channel_axis=2, fill_mode='constant', cval=0.0, order=1)
+	# flip first;
+	# create a temporary directory;
+	with tempfile.TemporaryDirectory() as prefix:
+		output_flip_path = os.path.join(prefix , "flipped_tmp.mp4")
+		# create the file within the temp directory;
+		ftools.check_newfile(output_flip_path)
 
+		# the dummy file has been created;
+		# safe to process;
+		VID.video_flip(input, output_flip_path)
+	
+		# rotation degree;
+		#DEGREE = [0, 3, 5, 7, 9, -3, -5, -7, -9]
 
-print("henshin ", image.shape)
-plt.imshow(transformation)
-plt.show()
-'''
-
-
-
-import numpy as np
-import cv2 as cv
-
-videopath = "C:\\CAPSTONE\\capstone2020\\src\\training\\test-videos\\auslan\\ambulance.mp4"
-cap = cv.VideoCapture(videopath)
-while cap.isOpened():
-    ret, frame = cap.read()
-    # if frame is read correctly ret is True
-    if not ret:
-        print("Can't receive frame (stream end?). Exiting ...")
-        break
-    transformation = tf.keras.preprocessing.image.apply_affine_transform(
-    frame, theta=0, tx=0, ty=0, shear=30, zx=1.1, zy=1.1, row_axis=0, col_axis=0,
-    channel_axis=2, fill_mode='constant', cval=0.0, order=1)
-    cv.imshow('frame', frame)
-    if cv.waitKey(1) == ord('q'):
-        break
-cap.release()
-cv.destroyAllWindows()
-'''
+		# assigning addresses for each rotation for convenienece;
+		addresses = assign_video_locations(DEGREE, storage_path, classname, speedname, "fr")
+		num_workers = mp.cpu_count()  
+		pool = mp.Pool(num_workers)
+		for index in range(len(DEGREE)):
+			pool.apply_async(func, args=(output_flip_path, addresses[index], DEGREE[index]))
+		pool.close()
+		pool.join()
+		# end of function;
