43711562a0851345f36c50894febad14aff19362
Matthew Yong <matthewyfy@gmail.com>
2020-10-18 15:58:11 +1100

    Adding Model.h5 files to master pipeline (#64)
    * need to debug cuda environment
    
    * as a sanity check
    
    * Adding new gitignore for training folder
    
    * Pushing new model for testing
    
    * Pushing updated models to github again
    
    Co-authored-by: yick <yongw4@student.unimelb.edu.au>

---
+++ src/training/training-files/down_sampling.py
@@ -0,0 +1,13 @@
+#/usr/bin/env/python
+# created by nebulaM78 team; capstone 2020;
+# downsampling the generated frames;
+
+
+import numpy as np
+import file_tools as ft
+
+
+arr = np.array([i for i in range(150)])
+filename = test_np.txt
+ft.npy2text(bytearray, filename)
+
